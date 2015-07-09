1e4a3730adc6a2e56ca0a3715575142fde86f184
MaxMEllon <mozi_kke@excite.co.jp>
2015-07-10 05:40:28 +0900

    apiからデータ取得

--- build/scripts/github_signature.js
+++ build/scripts/github_signature.js
@@ -1,14 +1,91 @@
 (function() {
-  var Signature;
+  var Github, Signature;
 
   Signature = React.createClass({
     render: function() {
-      return React.createElement("div", null);
+      var github, raw;
+      github = new Github();
+      raw = github.getUserData();
+      console.debug(raw);
+      return React.createElement("div", null, React.createElement("span", null));
     }
   });
 
   window.onload = function() {
-    return React.render(React.createElement(Signature, null), document.getElementById('content'));
+    return React.render(React.createElement(Signature, {
+      "username": 'maxmellon'
+    }), document.getElementById('content'));
   };
+
+  this.GithubApi = (function() {
+    GithubApi.server = 'https://api.github.com';
+
+    GithubApi.userPath = '/users/';
+
+    function GithubApi() {}
+
+    GithubApi.AjaxCall = function(username, option) {
+      if (option == null) {
+        option = null;
+      }
+      if (option == null) {
+        option = {};
+      }
+      if (option.type == null) {
+        option.type = 'GET';
+      }
+      if (option.params == null) {
+        option.params = {};
+      }
+      return $.ajax({
+        type: option.type,
+        url: this.server + this.userPath + username,
+        beforeSend: function() {
+          if (option.before != null) {
+            return option.before();
+          }
+        },
+        success: function(result, type) {
+          if (option.callback != null) {
+            return option.callback(result);
+          }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+          return console.error('Unknown');
+        }
+      });
+    };
+
+    GithubApi.prototype.getUserData = function(before, callback) {
+      var option;
+      return GithubApi.AjaxCall('maxmellon', option = {
+        type: 'GET',
+        before: before,
+        callback: callback
+      });
+    };
+
+    return GithubApi;
+
+  })();
+
+  Github = (function() {
+    function Github() {
+      this.before = function() {
+        return $('#content').html("loading now...");
+      };
+      this.callback = function(d) {
+        $('#content').empty();
+        return $('#content').text(d.users);
+      };
+    }
+
+    Github.prototype.getUserData = function() {
+      return new GithubApi().getUserData(this.before, this.callback);
+    };
+
+    return Github;
+
+  })();
 
 }).call(this);
--- src/scripts/github_signature.coffee
+++ src/scripts/github_signature.coffee
@@ -1,7 +1,51 @@
 Signature = React.createClass
   render: ->
+    github = new Github()
+    raw = github.getUserData()
+    console.debug raw
     <div>
+      <span>
+      </span>
     </div>
 
 window.onload = ->
-  React.render(<Signature />, document.getElementById('content'))
+  React.render <Signature username='maxmellon'/>, document.getElementById('content')
+
+class @GithubApi
+  @server = 'https://api.github.com'
+  @userPath = '/users/'
+
+  constructor: ->
+
+  @AjaxCall: (username, option = null) ->
+    option = {} unless option?
+    option.type = 'GET' unless option.type?
+    option.params = {} unless option.params?
+
+    $.ajax
+      type: option.type
+      url: @server + @userPath + username
+      beforeSend: ->
+        option.before() if option.before?
+      success: (result, type)->
+        option.callback(result) if option.callback?
+      error: (XMLHttpRequest, textStatus, errorThrown)->
+        console.error('Unknown')
+
+  getUserData: (before, callback) ->
+    GithubApi.AjaxCall 'maxmellon', option =
+      type: 'GET'
+      before: before
+      callback: callback
+
+class Github
+  constructor: ->
+    @before = ->
+      $('#content').html("loading now...")
+    @callback = (d) ->
+      $('#content').empty()
+      $('#content').text d.users
+
+  getUserData: ()->
+    return new GithubApi().getUserData(@before, @callback)
+
